7d513214cff541465ace3a432e435290fa2c57d7
kell <kellcrivelaro92@gmail.com>
2024-02-20 10:37:57 -0300

    fix: color

--- src/components/common/info-error.tsx
+++ src/components/common/info-error.tsx
@@ -37,21 +37,21 @@ const InfoError = ({
     <HoverCard openDelay={150} closeDelay={150} open={show}>
       <HoverCardTrigger>{children}</HoverCardTrigger>
       <HoverCardContent
-        className="relative w-fit px-4 py-2 text-sm text-secondary-foreground dark:bg-[#292724] 
-        dark:shadow-muted-foreground/10 lg:text-base"
+        className="relative w-fit border border-muted-foreground/30 px-4 py-2 text-sm text-secondary-foreground dark:bg-[#292724]
+        lg:text-base"
         {...contentProps}
       >
         {message}
         <div
           className={cn(
-            'clip-polygon absolute -bottom-3 h-[24px] w-[14px] bg-border',
+            'clip-polygon absolute -bottom-3 h-[24px] w-[14px] bg-muted-foreground/30',
             arrowSide === 'right' ? 'right-[15px]' : 'left-[15px]',
           )}
         />
         <div
           className={cn(
             'clip-polygon absolute -bottom-2.5 right-4 h-5 w-3 bg-popover dark:bg-[#292724]',
-            arrowSide === 'right' ? 'right-[15px]' : 'left-[16px]',
+            arrowSide === 'right' ? 'right-[16px]' : 'left-[16px]',
           )}
         />
       </HoverCardContent>
