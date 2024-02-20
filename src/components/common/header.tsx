7d08812b41be8621de192ef0eaa3eceb1631580c
kell <kellcrivelaro92@gmail.com>
2024-02-20 16:20:07 -0300

    feat: add favicon

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="container flex items-center justify-between py-3">
         <Link
           href="/"
-          className="texto-accent-foreground text-xl font-bold transition-all hover:scale-105 2xl:text-3xl"
+          className="text-xl font-bold text-accent-foreground transition-all hover:scale-105 2xl:text-3xl"
         >
           <div className="logo bg-gradient-to-bl from-primary to-green-800 py-3 pl-4 pr-5 lg:py-5 lg:pl-5 lg:pr-7">
             <span className="text-gray-200 dark:text-foreground">Polls</span>
